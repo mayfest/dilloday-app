0bf906f416ae8286d5a62c38530cb6480d219139
Ethan Pineda <joshuapineda66@gmail.com>
2025-01-19 00:40:52 -0600

    adding svg support

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,8 @@
 import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { StatusBar } from "expo-status-bar";
-
 export default function HomeScreen() {
   return (
-    <View className="flex-1 bg-background p-4">
+    <View className="flex-1 items-center justify-center bg-background p-4">
       <Text className="text-text-primary">Screen Content</Text>
     </View>
   );
---
+++ components/navigation/tab-bar-button.tsx
@@ -0,0 +1,57 @@
+import { FontAwesome6 } from "@expo/vector-icons";
+import { Image, TouchableOpacity } from "react-native";
+import BalloonLogo from "@/assets/images/balloonlogopink.svg";
+interface TabBarButtonProps {
+  focused: boolean;
+  icon: string;
+  navigation: any;
+  route: any;
+  home?: boolean;
+}
+
+export default function TabBarButton({
+  focused,
+  icon,
+  navigation,
+  route,
+  home,
+}: TabBarButtonProps) {
+  const onPress = () => {
+    const event = navigation.emit({
+      type: "tabPress",
+      target: route.key,
+      canPreventDefault: true,
+    });
+
+    if (!focused && !event.defaultPrevented) {
+      navigation.navigate(route.name, route.params);
+    }
+  };
+
+  const onLongPress = () => {
+    navigation.emit({
+      type: "tabLongPress",
+      target: route.key,
+    });
+  };
+
+  return (
+    <TouchableOpacity
+      className={`p-2 rounded-full w-[50px] max-w-[50px] h-[50px] flex-1 items-center justify-center ${
+        focused ? "bg-accent" : "bg-transparent"
+      }`}
+      onPress={onPress}
+      onLongPress={onLongPress}
+    >
+      {home ? (
+        <BalloonLogo width={32} height={32} />
+      ) : (
+        <FontAwesome6
+          name={icon}
+          size={20}
+          color={focused ? "#173885" : "#F6F2A3"}
+        />
+      )}
+    </TouchableOpacity>
+  );
+}
