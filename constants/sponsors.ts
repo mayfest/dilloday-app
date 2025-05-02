15b37efcaafdfd303e8ec94973c8a2098b129cf5
Ethan Pineda <joshuapineda66@gmail.com>
2025-05-02 00:56:03 -0500

    sponsors page

---
+++ constants/sponsors.ts
@@ -0,0 +1,86 @@
+export const SPONSORS: {
+  name: string;
+  logo: any;
+  url?: string;
+}[] = [
+  {
+    name: 'Guzman y Gomez',
+    logo: require('@/assets/images/company-logos/guzman-y-gomez.png'),
+    url: 'https://www.guzmanygomez.com',
+  },
+  {
+    name: 'Animal Records',
+    logo: require('@/assets/images/company-logos/animal-records.png'),
+    url: 'https://animal-records.com',
+  },
+  {
+    name: 'Chicago White Sox',
+    logo: require('@/assets/images/company-logos/chicago-white-sox.png'),
+    url: 'https://www.mlb.com/whitesox',
+  },
+  {
+    name: 'Coasta Coffee',
+    logo: require('@/assets/images/company-logos/coasta-coffee.png'),
+    url: 'https://www.us.costacoffee.com/',
+  },
+  {
+    name: 'Crossroads',
+    logo: require('@/assets/images/company-logos/crossroads.png'),
+    url: 'https://crossroads.com',
+  },
+  {
+    name: 'L’Oreal',
+    logo: require('@/assets/images/company-logos/l-oreal.png'),
+    url: 'https://www.loreal.com',
+  },
+  {
+    name: 'Farmer’s Fridge',
+    logo: require('@/assets/images/company-logos/farmers-fridge.png'),
+    url: 'https://www.farmersfridge.com',
+  },
+  {
+    name: 'Line Leap',
+    logo: require('@/assets/images/company-logos/line-leap.jpeg'),
+    url: 'https://www.lineleap.com/',
+  },
+  {
+    name: 'Lifetime',
+    logo: require('@/assets/images/company-logos/line.jpeg'),
+    url: 'https://www.lifetime.life/locations/il/evanston.html',
+  },
+  {
+    name: 'Partiful',
+    logo: require('@/assets/images/company-logos/partier.png'),
+    url: 'https://partiful.com/events',
+  },
+  {
+    name: 'Photobooth',
+    logo: require('@/assets/images/company-logos/photobooth-awh.jpg'),
+    url: 'https://photobooth.awh.co.id/index.html',
+  },
+  {
+    name: 'Pretty Cool',
+    logo: require('@/assets/images/company-logos/pretty-cool.png'),
+    url: 'https://pretty-cool.com',
+  },
+  {
+    name: 'Red Bull',
+    logo: require('@/assets/images/company-logos/red-bull.png'),
+    url: 'https://www.redbull.com',
+  },
+  {
+    name: 'Sprite',
+    logo: require('@/assets/images/company-logos/sprite-logo.png'),
+    url: 'https://www.sprite.com',
+  },
+  {
+    name: 'Vitamin Water',
+    logo: require('@/assets/images/company-logos/vitamin-water.jpg'),
+    url: 'https://www.vitaminwater.com',
+  },
+  {
+    name: 'LOOK app',
+    logo: require('@/assets/images/company-logos/LOOK-app.png'),
+    url: 'https://www.looktheapp.com/',
+  },
+];
