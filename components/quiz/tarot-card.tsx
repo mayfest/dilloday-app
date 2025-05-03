f7d8bfea5bb974e5da1103ca6d51d295b43a0558
crose-prw <crose.prw@gmail.com>
2025-05-03 14:35:12 -0500

    Basic tarot card functioning working

--- components/quiz/tarot-card.tsx
+++ components/quiz/tarot-card.tsx
@@ -2,29 +2,74 @@
 // VISION: tarot card on the front (visual with svg) and description on the back
 
 // import ReactNative from 'react-native';
+import TarotSun from '@/components/quiz/card-sun';
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import RawCardFlip from 'react-native-card-flip';
+const CardFlip: any = RawCardFlip;
 
 // IMPORT THIS!
 // https://www.npmjs.com/package/react-native-card-flip
 
+// import React from 'react';
+
+
+
+/* */
+
 interface TarotCardProps {
-  image: any
-  alt: string
-  desc: string
+  svg: React.FC<React.SVGProps<SVGSVGElement>>,
+  // image: any
+  // alt: string,
+  desc: string,
 
 }
+
+class TarotCard extends React.Component<TarotCardProps> {
+  card: CardFlip | null = null;
+
+  flipCard = () => {
+    this.card?.flip();
+  };
+
+  render() {
+    const { svg, desc } = this.props;
+
+    return (
+      <CardFlip
+        style={styles.cardContainer}
+        ref={(ref) => (this.card = ref)}
+      >
+        <TouchableOpacity style={styles.card} onPress={this.flipCard}>
+          <TarotSun/>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card} onPress={this.flipCard}>
+          <Text>{desc}</Text>
+        </TouchableOpacity>
+      </CardFlip>
+      
+    );
+  }
+}
+
+
+export default TarotCard;
+
+/*
 
 export default function TarotCard ({image, desc, alt}: TarotCardProps) {
   return (
-    <Text>TEST</Text>
-    // <CardFlip style={styles.cardContainer} ref={(card) => this.card = card} >
-    //   <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text>AB</Text></TouchableOpacity>
-    //   <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text>CD</Text></TouchableOpacity>
-    // </CardFlip>
+    // <Text>TEST</Text>
+    <CardFlip style={styles.cardContainer} ref={(card) => this.card = card} >
+      <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text>AB</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text>CD</Text></TouchableOpacity>
+    </CardFlip>
 
   );
 }
+
+*/
 
 const styles = StyleSheet.create({
   cardContainer: {
